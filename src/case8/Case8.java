4b9632551b0eb31a2d4671e4f5167f5b7af3cc57
Edgar <edgar.mata.1911@gmail.com>
2019-10-04 13:59:20 -0600

    ErroresRarosDeGit

--- src/case8/Case8.java
+++ src/case8/Case8.java
@@ -1,13 +1,17 @@
 package case8;
 
+import java.awt.Color;
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class Case8 
 {
     public static void main(String[] args) throws IOException 
     {
     	ImageReader imageReader = new ImageReader();
-        imageReader.getImageSectorInformation(new File("C:\\Users\\Edgar\\Desktop\\Importante\\TEC\\IV SEMESTRE\\ANALISIS DE ALGORITMOS\\pruebaImg.jpg"),0.3);
+        ArrayList<PixelInformation> pixelsInformation = imageReader.getImagePixelsInformation(new File("C:\\Users\\Edgar\\Desktop\\Importante\\TEC\\IV SEMESTRE\\ANALISIS DE ALGORITMOS\\pruebaImg.jpg"),0.3);    
+        ArrayList<Color> colorPerSector = imageReader.getAVG(pixelsInformation);
+		imageReader.getImageSectorInformation(new File("C:\\Users\\Edgar\\Desktop\\Importante\\TEC\\IV SEMESTRE\\ANALISIS DE ALGORITMOS\\pruebaImg.jpg"),0.3);
     }
 }
--- src/case8/ImageReader.java
+++ src/case8/ImageReader.java
@@ -5,7 +5,6 @@ import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.HashMap;
 
 import javax.imageio.ImageIO;
 
@@ -20,7 +19,7 @@ public class ImageReader
         int numberOfPixelsPerSector = (int) (pSectorPixelsPercentage * SECTOR_SIZE);
         PixelInformation[] pixelsInformation = new PixelInformation[numberOfPixelsPerSector*SECTOR_SIZE];
         int currentSector = 0;
-        int pixelIndex = 0;
+		int pixelIndex = 0;
 		for(int row = 0;row<DIMENSION;row++) {
 			for(int column = 0;column<DIMENSION;column++) {
 		        for(int currentPixel = 0;currentPixel<numberOfPixelsPerSector;currentPixel++) {
@@ -34,6 +33,39 @@ public class ImageReader
 			}
 		}
         return pixelsInformation;
+    }
+
+    public ArrayList<Color> getAVG(ArrayList<PixelInformation> pixelsInformation) {
+        //recorrer cada elemento del Array hasta cambiar de sector
+        int totalRED = 0;
+        int totalGREEN = 0;
+        int totalBLUE = 0;
+        int cont = 0;
+        int numberOfPixelsPerSector = (int) (0.3 * SECTOR_SIZE);
+        ArrayList<Color> colorPerSector = new ArrayList<>();
+
+        for (int element = 0; element < pixelsInformation.size(); element++) {
+            int red = pixelsInformation.get(element).getColor().getRed();
+            totalRED += red;
+            int green = pixelsInformation.get(element).getColor().getGreen();
+            totalGREEN += green;
+            int blue = pixelsInformation.get(element).getColor().getBlue();
+            totalBLUE += blue;
+            cont++;
+            if (cont == numberOfPixelsPerSector) {
+                int avgRed = totalRED / numberOfPixelsPerSector;
+                int avgGreen = totalGREEN / numberOfPixelsPerSector;
+                int avgBlue = totalBLUE / numberOfPixelsPerSector;
+                Color sector = new Color(avgRed, avgGreen, avgBlue);
+                colorPerSector.add(sector);
+                totalRED = 0;
+                totalGREEN = 0;
+                totalBLUE = 0;
+                cont = 0;
+            }
+        }
+
+        return colorPerSector;
     }
 	public SectorInformation[][] getImageSectorInformation(File pFile, double pSectorPixelsPercentage) throws IOException
 	{
