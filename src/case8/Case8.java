7fed3d8284adba2c4c5e56e835fb105bf1404e1e
Mario Fernandez <mariosmite123@gmail.com>
2019-10-14 15:25:38 -0600

    FixVariables/No Restriccion

--- src/case8/Case8.java
+++ src/case8/Case8.java
@@ -4,6 +4,6 @@ public class Case8 {
 
     public static void main(String[] args) {
     	HtmlWriter htmlWriter = new HtmlWriter();
-    	htmlWriter.drawPolygons("C:\\Users\\mario\\OneDrive\\Desktop\\Caso8Netbeans\\src\\case8\\html.html", "C:\\Users\\mario\\OneDrive\\Desktop\\imagenes\\goku.png","C:\\Users\\mario\\OneDrive\\Desktop\\imagenes\\vegeta.png","C:\\Users\\mario\\OneDrive\\Desktop\\imagenes\\meliodas.png", 0.3);
+    	htmlWriter.drawPolygons("C:\\Users\\mario\\OneDrive\\Desktop\\Caso8Netbeans\\src\\case8\\html.html", "C:\\Users\\mario\\OneDrive\\Desktop\\imagenes\\goku.png","C:\\Users\\mario\\OneDrive\\Desktop\\imagenes\\vegeta.png","C:\\Users\\mario\\OneDrive\\Desktop\\imagenes\\deku.jpg", 0.3);
     }
 }
